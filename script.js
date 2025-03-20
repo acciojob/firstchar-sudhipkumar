1b3ccea326c5bdc52fcf9cbb140e8d75a51e2a6a
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-03-20 10:57:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function firstChar(text) {
 }
 
 // Do not change the code below
+
 // Uncomment the following lines to test in a browser environment
-// const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 // alert(firstChar(text));
